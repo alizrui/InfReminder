80a7fd38ea421b25c0d778beab887ab8aef655db
alizrui <47398821+alizrui@users.noreply.github.com>
2021-03-31 18:54:30 +0200

    por capas main

--- app/src/main/java/com/example/infreminder/activities/MainActivity.java
+++ app/src/main/java/com/example/infreminder/activities/MainActivity.java
@@ -8,60 +8,34 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.infreminder.R;
+import com.example.infreminder.activities.interfaces.I_MainActivity;
+import com.example.infreminder.activitiespresenter.MainActivityPresenter;
+import com.example.infreminder.activitiespresenter.interfaces.I_MainActivityPresenter;
 import com.example.infreminder.fragmentsview.CreateAlarmFragment;
 import com.example.infreminder.fragmentsview.ReminderListFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements I_MainActivity {
+
+    private I_MainActivityPresenter logic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        logic = new MainActivityPresenter(this);
+
         //comentario
     }
 
     public void updateFragments(View view) {
-        Class<? extends Fragment> fragmentToAdd = null;
-        Fragment fragmentToRemove = null; // not used yet
-
-        Bundle bundle = null;
-        int layout = 0;
 
         final int clickedButton = view.getId();
+        logic.updateFragments(clickedButton);
+    }
 
-
-        if (clickedButton == R.id.bCreate){
-            // Keep a reference to the FragmentContainerView to be used to add the Fragment
-            layout = R.id.fcvFragment;
-            // Get a reference to the Fragment class
-            fragmentToAdd = CreateAlarmFragment.class;
-
-            bundle = new Bundle();
-        } else
-
-
-        if (clickedButton == R.id.bShow) {
-
-            layout = R.id.fcvFragment;
-            // Get a reference to the Fragment class
-            fragmentToAdd = ReminderListFragment.class;
-
-        }
-        // Get a FragmentTransaction to begin some operations with the current FragmentManager
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.setReorderingAllowed(true);
-        // Remove the required Fragment NOT USED YET
-        if (fragmentToRemove != null) {
-            transaction.remove(fragmentToRemove);
-        }
-        // Replace the Fragments in the required Layout by the selected one
-        if (fragmentToAdd != null) {
-            transaction.replace(layout, fragmentToAdd, bundle);
-        }
-        // Add the transaction to the BackStack, so it can be reversed by pressing the Back button
-        transaction.addToBackStack(null);
-        // Make changes effective
-        transaction.commit();
+    @Override
+    public MainActivity getMainActivity() {
+        return this;
     }
 }
---
+++ app/src/main/java/com/example/infreminder/activitiespresenter/interfaces/I_MainActivityPresenter.java
@@ -0,0 +1,6 @@
+package com.example.infreminder.activitiespresenter.interfaces;
+
+public interface I_MainActivityPresenter  {
+
+    void updateFragments(int idButton);
+}
