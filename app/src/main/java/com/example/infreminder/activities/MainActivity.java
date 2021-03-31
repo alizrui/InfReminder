3ffc0ed347fea3753a2fc1305d5ce0eb1f6db13c
fiordor <1fiordor@gmail.com>
2021-03-31 17:54:16 +0200

    Implementado loas interfaces de los view

--- app/src/main/java/com/example/infreminder/activities/MainActivity.java
+++ app/src/main/java/com/example/infreminder/activities/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //comentario
     }
 
     public void updateFragments(View view) {
---
+++ app/src/main/java/com/example/infreminder/fragments/interfaces/I_CreateReminderFragment.java
@@ -0,0 +1,4 @@
+package com.example.infreminder.fragments.interfaces;
+
+public interface I_CreateReminderFragment {
+}
