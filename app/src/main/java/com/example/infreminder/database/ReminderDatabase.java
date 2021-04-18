8873bcb218806c4565473dbef13ef137c3ae6ee5
samcl94 <claessammyjonathan@gmail.com>
2021-04-18 20:33:26 +0200

    Base de datos con segundo planos y lógica de crear recordatorio terminada

--- app/src/main/java/com/example/infreminder/database/ReminderDatabase.java
+++ app/src/main/java/com/example/infreminder/database/ReminderDatabase.java
@@ -17,7 +17,6 @@ public abstract class ReminderDatabase extends RoomDatabase {
         if (reminderDatabase == null) {
             reminderDatabase = Room
                     .databaseBuilder(context, ReminderDatabase.class, "reminder_database")
-                    .allowMainThreadQueries()
                     .build();
         }
         return reminderDatabase;
--- app/src/main/java/com/example/infreminder/fragmentsview/CreateReminderFragment.java
+++ app/src/main/java/com/example/infreminder/fragmentsview/CreateReminderFragment.java
@@ -2,13 +2,19 @@ package com.example.infreminder.fragmentsview;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
@@ -18,20 +24,66 @@ import com.example.infreminder.reminder.Reminder;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import org.w3c.dom.Text;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class CreateReminderFragment extends Fragment {
-    TextInputLayout tiDate;
-    TextInputEditText tieDate;
+    private TextInputLayout tiDate;
+    private TextInputEditText tieDate;
+    private TextInputEditText tieName;
+    private TextInputEditText tieDescription;
+    private Button bAccept;
+    private Button bCancel;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_create_reminder, container, false);
         tiDate = (TextInputLayout) view.findViewById(R.id.prueba);
         tieDate = (TextInputEditText) view.findViewById(R.id.tIDay);
-        tiDate.setError("La fecha no tiene el formato correcto");
+        tieName = (TextInputEditText) view.findViewById(R.id.tIName);
+        tieDescription = (TextInputEditText) view.findViewById(R.id.tIDescription);
+        bAccept = (Button) view.findViewById(R.id.bAccept);
+        bCancel =(Button) view.findViewById(R.id.bCancel);
+
+        tieDate.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                bAccept.setEnabled(fieldEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        tieName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                bAccept.setEnabled(fieldEmpty());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
         tieDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,17 +91,65 @@ public class CreateReminderFragment extends Fragment {
             }
         });
 
+        bAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE);
+                int diff = 0;
+                try {
+                    Date firstDate = sdf.parse("01/01/2020");
+                    Date secondDate = sdf.parse(tieDate.getText().toString());
+                    diff = (int) (secondDate.getTime() - 4 - firstDate.getTime());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                int finalDiff = diff;
+                new Thread(() -> {
+
+                    Reminder reminder = new Reminder(finalDiff,
+                            tieName.getText().toString(),
+                            tieDescription.toString(),"",
+                            Calendar.getInstance());
+                    ReminderDatabase.getInstance(getContext()).reminderDao().addReminder(reminder);
+                }).start();
+                getActivity().onBackPressed();
+
+            }
+        });
+
+        bCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle(R.string.cancel);
+                builder.setMessage(R.string.title_cancel);
+                builder.setPositiveButton(R.string.aceptar, (dialog, which) -> {
+                    getActivity().onBackPressed();
+                });
+                builder.setNegativeButton(R.string.cancel, null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        });
+
         return view;
+    }
+    private boolean fieldEmpty() {
+        String s1 = tieDate.getText().toString();
+        String s2 = tieName.getText().toString();
+        return (s1.isEmpty() || s2.isEmpty()) ? false : true;
     }
     private void showDatePickerDialog() {
         DatePickerFragment newFragment = DatePickerFragment.newInstance((datePicker, year, month, day) -> {
+            String s1 = (day / 10 < 1) ? "0": "";
+            String s2 = ((month + 1)  / 10 < 1) ? "0": "";
             // +1 because January is zero
-            final String selectedDate = day + "/" + (month + 1) + "/" + year;
+            final String selectedDate = s1 + day + "/" + s2 + (month + 1) + "/" + year;
             tieDate.setText(selectedDate);
         });
         newFragment.show(getActivity().getSupportFragmentManager(), "datePicker");
     }
-
     public static class DatePickerFragment extends DialogFragment {
 
         private DatePickerDialog.OnDateSetListener listener;
