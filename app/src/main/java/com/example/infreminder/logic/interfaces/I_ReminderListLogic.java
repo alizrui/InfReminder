b9598f71e1bac7580f088f5d09a29415e3ea3768
samcl94 <claessammyjonathan@gmail.com>
2021-05-02 15:15:44 +0200

    ListView arreglado

--- app/src/main/java/com/example/infreminder/logic/interfaces/I_ReminderListLogic.java
+++ app/src/main/java/com/example/infreminder/logic/interfaces/I_ReminderListLogic.java
@@ -1,4 +1,15 @@
 package com.example.infreminder.logic.interfaces;
 
+import androidx.fragment.app.Fragment;
+
+import com.example.infreminder.pojo.Reminder;
+import com.example.infreminder.view.ReminderListView;
+
+import java.util.List;
+
 public interface I_ReminderListLogic {
+    /**
+     * Actualiza la lista con los nuevos recordatorios insertados
+     */
+    void updateReminders();
 }
